b47b779f385a8774415380c6904686a1b65bae6f
Void <143309999+VVoiddd@users.noreply.github.com>
2024-06-11 22:11:47 -0400

    Create shared_utils.py

---
+++ shared_utils.py
@@ -0,0 +1,14 @@
+import os
+
+def get_core_windows_dirs():
+    return [
+        os.environ.get('SystemRoot', 'C:\\Windows'),
+        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Program Files'),
+        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Program Files (x86)'),
+        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Users', 'Default'),
+        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Users', 'Public'),
+        os.path.join(os.environ.get('SystemDrive', 'C:'), '$Recycle.Bin'),
+        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Recovery'),
+        os.path.join(os.environ.get('SystemDrive', 'C:'), 'System Volume Information'),
+        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Windows.old')
+    ]
