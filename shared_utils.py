2b3b8e2022fcd809c39415bcd5f35a6f2a3ead06
Void <143309999+VVoiddd@users.noreply.github.com>
2024-06-17 01:01:10 -0400

    Update shared_utils.py

--- shared_utils.py
+++ shared_utils.py
@@ -1,16 +1,23 @@
-#shared_utils.py
-
 import os
 
 def get_core_windows_dirs():
+    """
+    Get a list of core Windows system directories that should be skipped during file operations.
+
+    Returns:
+    list: List of core Windows system directory paths.
+    """
+    system_root = os.environ.get('SystemRoot', 'C:\\Windows')
+    system_drive = os.environ.get('SystemDrive', 'C:')
+
     return [
-        os.environ.get('SystemRoot', 'C:\\Windows'),
-        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Program Files'),
-        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Program Files (x86)'),
-        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Users', 'Default'),
-        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Users', 'Public'),
-        os.path.join(os.environ.get('SystemDrive', 'C:'), '$Recycle.Bin'),
-        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Recovery'),
-        os.path.join(os.environ.get('SystemDrive', 'C:'), 'System Volume Information'),
-        os.path.join(os.environ.get('SystemDrive', 'C:'), 'Windows.old')
+        system_root,
+        os.path.join(system_drive, 'Program Files'),
+        os.path.join(system_drive, 'Program Files (x86)'),
+        os.path.join(system_drive, 'Users', 'Default'),
+        os.path.join(system_drive, 'Users', 'Public'),
+        os.path.join(system_drive, '$Recycle.Bin'),
+        os.path.join(system_drive, 'Recovery'),
+        os.path.join(system_drive, 'System Volume Information'),
+        os.path.join(system_drive, 'Windows.old')
     ]
